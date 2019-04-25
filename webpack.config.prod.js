9e43ffc3545b3162796aac855b374dbecc365a8c
zhengbigbig <780357902@qq.com>
2019-04-25 17:52:50 +0800

    配置生产环境的webpack配置 externals说明

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,12 +1,13 @@
 const base = require('./webpack.config')
 module.exports = Object.assign({}, base, {
     mode: 'production',
+    //外部的库，对应js不同打包工具
     externals: {
         react: {
             commonjs: 'react',
-            commonjs2: 'react',
-            amd: 'react',
-            root: 'React',
+            commonjs2: 'react',  //nodejs  modules.export
+            amd: 'react',       //浏览器标准
+            root: 'React',      // 两者都不是 <script src="xx/xx"></script>  window.xxx
         },
         'react-dom': {
             commonjs: 'react-dom',
