6dbfe42e63cd10235cb377b15b4c7a9371e00457
zhengbigbig <780357902@qq.com>
2019-04-29 17:50:47 +0800

    增加footer okText cancelText onCancel onOk maskClosable 等参数 完成点击事件

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -3,18 +3,26 @@ import Dialog from './dialog';
 
 export default function () {
   const [x, setX] = useState(false);
-  const onClick = () => {
-    console.log(1);
+  const onClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     setX(!x);
+  };
+  const onOk = () => {
+    setX(false);
+  };
+  const onCancel = () => {
+    setX(false);
   };
   return (
     <div>
       <button onClick={onClick}>click</button>
       <Dialog
         visible={x}
-
         title={<span>xxx</span>}
+        onOk={onOk}
+        // maskClosable={false}
+        onCancel={onCancel}
       >
+
         <div>xxxxxxxxxxx</div>
       </Dialog>
     </div>
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,34 +1,53 @@
 import React, {Fragment, ReactNode} from 'react';
-import handlePrefix  from '../addPrefix'
+import handlePrefix from '../addPrefix';
 import './dialog.less';
 import {Icon} from '../index';
-
-
-interface DialogProps {
-  visible: boolean;
-  width?: string | number;
-  title: string | ReactNode
-
-}
 
 const addPrefix = handlePrefix('hdd-dialog');
 const ap = addPrefix;
 
+interface DialogProps {
+  visible: boolean;
+  width?: string | number;
+  title: string | ReactNode;
+  footer?: null | ReactNode;
+  okText?: string | ReactNode;
+  cancelText?: string | ReactNode;
+  onCancel: React.MouseEventHandler;
+  onOk: React.MouseEventHandler;
+  maskClosable?: boolean;
+}
+
 const Dialog: React.FunctionComponent<DialogProps> =
-  ({width, visible, children, title, ...restProps}) => {
+  ({
+     width, visible, children, title,
+     footer, okText, cancelText, onOk, onCancel, maskClosable, ...restProps
+   }) => {
     const dialogWidth =
       typeof width === 'string' ? width :
         typeof width === 'number' ? `${width}px` :
           '520px';
+    const onClickOpen: React.MouseEventHandler = (e) => {
+      onOk(e);
+    };
+    const onClickClose: React.MouseEventHandler = (e) => {
+      onCancel(e);
+    };
+    const onClickMaskClose: React.MouseEventHandler = (e) => {
+      if (maskClosable) {
+        onCancel(e);
+      }
+    };
+    console.log(maskClosable);
     return (
       visible ?
         <Fragment>
           <div className={ap('mask')}>
           </div>
-          <div className={ap('wrap')}>
+          <div className={ap('wrap')}  onClick={onClickMaskClose}>
             <div className={ap('')} style={{width: dialogWidth}} {...restProps}>
               <div className={ap('content')}>
-                <button className={ap('close')}>
+                <button className={ap('close')} onClick={onClickClose}>
                   <span className={ap('close-x')}>
                     <Icon name="close"/>
                   </span>
@@ -40,9 +59,18 @@ const Dialog: React.FunctionComponent<DialogProps> =
                 <div className={ap('body')}>
                   {children}
                 </div>
-                <div className={ap('footer')}>
+                {footer === null ? null :
+                  <div className={ap('footer')}>
+                    {footer ?
+                      footer :
+                      <Fragment>
+                        <button onClick={onClickClose}>{cancelText || '取消'}</button>
+                        <button style={{marginLeft: '8px'}} onClick={onClickOpen}>{okText || '确认'}</button>
+                      </Fragment>
+                    }
+                  </div>
+                }
 
-                </div>
               </div>
 
             </div>
@@ -52,6 +80,7 @@ const Dialog: React.FunctionComponent<DialogProps> =
     );
   };
 Dialog.defaultProps = {
-  width: '520px'
+  width: '520px',
+  maskClosable: true
 };
 export default Dialog;
