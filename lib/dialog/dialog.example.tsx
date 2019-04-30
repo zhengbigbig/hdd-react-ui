cd29919bc0ab8ec388bc6e02b463a80a5dd57510
zhengbigbig <780357902@qq.com>
2019-04-30 16:48:15 +0800

    初步完成dialog组件 1、高阶函数 2、传送门Portal 3、动态生成组件 4、闭包传api

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,8 +1,9 @@
 import React, {useState} from 'react';
-import Dialog from './dialog';
+import Dialog, {alert, confirm} from './dialog';
 
 export default function () {
   const [x, setX] = useState(false);
+  const [y, setY] = useState(false);
   const onClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     setX(!x);
   };
@@ -12,19 +13,69 @@ export default function () {
   const onCancel = () => {
     setX(false);
   };
+  const onOky = () => {
+    setY(false);
+  };
+  const onCancely = () => {
+    setY(false);
+  };
   return (
     <div>
-      <button onClick={onClick}>click</button>
-      <Dialog
-        visible={x}
-        title={<span>xxx</span>}
-        onOk={onOk}
-        // maskClosable={false}
-        onCancel={onCancel}
-      >
+      <div style={{zIndex: 9}}>
+        <button onClick={onClick}>click</button>
+        <Dialog
+          visible={x}
+          title="xxx"
+          onOk={onOk}
+          // maskClosable={false}
+          onCancel={onCancel}
+          zIndex={999}
+        >
+        </Dialog>
+      </div>
+      <div style={{zIndex: 10}}>
+        <button onClick={onClick}>click</button>
+        <Dialog
+          visible={y}
+          title={<span>yyy</span>}
+          onOk={onOky}
+          // maskClosable={false}
+          onCancel={onCancely}
+          zIndex={888}
+        >
+        </Dialog>
+      </div>
+      <button onClick={() => {
+        confirm(
+          {
+            title: 'xxx',
+            content: <button>click</button>,
+            onOk: () => {
+              console.log('ok');
+            },
+            onCancel: () => {
+              console.log('cancel');
+            }
+          }
+        );
+      }}>confirm
+      </button>
+      <button onClick={() => {
+        alert(
+          {
+            title: 'xxx',
+            content: <button>click</button>,
+            onOk: () => {
+              console.log('ok');
+            },
+            onCancel: () => {
+              console.log('cancel');
+            }
+          }
+        );
+      }}>alert
+      </button>
 
-        <div>xxxxxxxxxxx</div>
-      </Dialog>
     </div>
   );
 }
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,4 +1,5 @@
-import React, {Fragment, ReactNode} from 'react';
+import React, {Fragment, ReactFragment, ReactNode} from 'react';
+import ReactDOM from 'react-dom';
 import handlePrefix from '../addPrefix';
 import './dialog.less';
 import {Icon} from '../index';
@@ -9,19 +10,20 @@ const ap = addPrefix;
 interface DialogProps {
   visible: boolean;
   width?: string | number;
-  title: string | ReactNode;
+  title: ReactNode;
   footer?: null | ReactNode;
-  okText?: string | ReactNode;
-  cancelText?: string | ReactNode;
+  okText?: ReactNode;
+  cancelText?: ReactNode | null;
   onCancel: React.MouseEventHandler;
   onOk: React.MouseEventHandler;
   maskClosable?: boolean;
+  zIndex?: number;
 }
 
 const Dialog: React.FunctionComponent<DialogProps> =
   ({
      width, visible, children, title,
-     footer, okText, cancelText, onOk, onCancel, maskClosable, ...restProps
+     footer, okText, cancelText, onOk, onCancel, maskClosable, zIndex, ...restProps
    }) => {
     const dialogWidth =
       typeof width === 'string' ? width :
@@ -38,49 +40,131 @@ const Dialog: React.FunctionComponent<DialogProps> =
         onCancel(e);
       }
     };
-    console.log(maskClosable);
-    return (
-      visible ?
-        <Fragment>
-          <div className={ap('mask')}>
-          </div>
-          <div className={ap('wrap')}  onClick={onClickMaskClose}>
-            <div className={ap('')} style={{width: dialogWidth}} {...restProps}>
-              <div className={ap('content')}>
-                <button className={ap('close')} onClick={onClickClose}>
+    const template = visible ?
+      <Fragment>
+        <div className={ap('mask')} onClick={onClickMaskClose}>
+        </div>
+        <div className={ap('wrap')}>
+          <div className={ap('')} style={{width: dialogWidth, zIndex}} {...restProps}>
+            <div className={ap('content')}>
+              <button className={ap('close')} onClick={onClickClose}>
                   <span className={ap('close-x')}>
                     <Icon name="close"/>
                   </span>
 
-                </button>
-                <div className={ap('header')}>
-                  {title}
-                </div>
-                <div className={ap('body')}>
-                  {children}
-                </div>
-                {footer === null ? null :
-                  <div className={ap('footer')}>
-                    {footer ?
-                      footer :
-                      <Fragment>
-                        <button onClick={onClickClose}>{cancelText || '取消'}</button>
-                        <button style={{marginLeft: '8px'}} onClick={onClickOpen}>{okText || '确认'}</button>
-                      </Fragment>
-                    }
-                  </div>
-                }
-
+              </button>
+              <div className={ap('header')}>
+                {title}
               </div>
+              <div className={ap('body')}>
+                {children}
+              </div>
+              {footer === null ? null :
+                <div className={ap('footer')}>
+                  {footer ?
+                    footer :
+                    <Fragment>
+                      {cancelText === null ? null : <button onClick={onClickClose}>{cancelText || '取消'}</button>}
+                      <button style={{marginLeft: '8px'}} onClick={onClickOpen}>{okText || '确认'}</button>
+                    </Fragment>
+                  }
+                </div>
+              }
+
 
             </div>
+
           </div>
-        </Fragment>
-        : null
+        </div>
+      </Fragment>
+      : null;
+    return (
+      ReactDOM.createPortal(template, document.body)
     );
   };
 Dialog.defaultProps = {
   width: '520px',
-  maskClosable: true
+  maskClosable: true,
+  zIndex: 1000
 };
+
+
+const confirm = (
+  {title, content, onOk, onCancel}: {
+    title: ReactNode,
+    content: ReactNode,
+    onOk?: () => void,
+    onCancel?: () => void,
+    okText?: ReactNode,
+    cancelText?: ReactNode,
+  }
+): void => {
+  const onYes = () => {
+    ReactDOM.render(React.cloneElement(component, {visible: false}), div);
+    ReactDOM.unmountComponentAtNode(div);
+    div.remove();
+    onOk && onOk();
+  };
+  const onNo = () => {
+    ReactDOM.render(React.cloneElement(component, {visible: false}), div);
+    ReactDOM.unmountComponentAtNode(div);
+    div.remove();
+    onCancel && onCancel();
+  };
+  const component =
+    <Dialog
+      visible={true}
+      title={title}
+      onOk={onYes}
+      onCancel={onNo}
+      maskClosable={false}
+    >
+      {content}
+
+    </Dialog>;
+  const div = document.createElement('div');
+  document.body.appendChild(div);
+  ReactDOM.render(component, div);
+};
+const alert = (
+  {title, content, onOk, onCancel, okText, cancelText}: {
+    title: ReactNode,
+    content: ReactNode | ReactFragment,
+    onOk?: () => void,
+    onCancel?: () => void,
+    okText?: ReactNode,
+    cancelText?: ReactNode,
+  }
+) => {
+  const onYes = () => {
+    ReactDOM.render(React.cloneElement(component, {visible: false}), div);
+    ReactDOM.unmountComponentAtNode(div);
+    div.remove();
+    onOk && onOk();
+  };
+  const onNo = () => {
+    ReactDOM.render(React.cloneElement(component, {visible: false}), div);
+    ReactDOM.unmountComponentAtNode(div);
+    div.remove();
+    onCancel && onCancel();
+  };
+  const component =
+    <Dialog
+      visible={true}
+      title={title}
+      onOk={onYes}
+      onCancel={onNo}
+      maskClosable={false}
+      cancelText={cancelText || null}
+      okText={okText || '知道了'}
+    >
+      {content}
+
+    </Dialog>;
+  const div = document.createElement('div');
+  document.body.appendChild(div);
+  ReactDOM.render(component, div);
+};
+
+export {alert, confirm};
 export default Dialog;
