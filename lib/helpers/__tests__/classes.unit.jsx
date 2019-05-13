85150a4f8cabf4c2f0cd65aefc167ef936559ed0
zhengbigbig <780357902@qq.com>
2019-05-13 18:36:59 +0800

    添加scopedClassMaker的测试用例

--- lib/helpers/__tests__/classes.unit.jsx
+++ lib/helpers/__tests__/classes.unit.jsx
@@ -1,4 +1,6 @@
 import classes from '../classes'
+import {scopedClassMaker} from "../classes";
+
 describe('classes', () => {
   it('接受 1 个 className', () => {
     const result = classes('a')
@@ -21,5 +23,30 @@ describe('classes', () => {
   it('接受 0 个参数', ()=>{
     const result = classes()
     expect(result).toEqual('')
+  })
+  it('scopedClassMaker接受一个string', ()=>{
+    const ap = scopedClassMaker('hdd-button')
+    const result = ap('')
+    expect(result).toEqual('hdd-button')
+  })
+  it('scopedClassMaker接受一个string', ()=>{
+    const ap = scopedClassMaker('hdd-button')
+    const result = ap('xxx')
+    expect(result).toEqual('hdd-button-xxx')
+  })
+  it('scopedClassMaker options接受一个额外的className', ()=>{
+    const ap = scopedClassMaker('hdd-button')
+    const result = ap('x',{extra:'xxx'})
+    expect(result).toEqual('hdd-button-x xxx')
+  })
+  it('scopedClassMaker name接受一个对象', ()=>{
+    const ap = scopedClassMaker('hdd-button')
+    const result = ap({x:true,y:false},{extra:'xxx'})
+    expect(result).toEqual('hdd-button-x xxx')
+  })
+  it('scopedClassMaker name接受一个数组', ()=>{
+    const ap = scopedClassMaker('hdd-button')
+    const result = ap(['xxx','yyy','zzz'],{extra:'xxx'})
+    expect(result).toEqual('hdd-button-xxx hdd-button-yyy hdd-button-zzz xxx')
   })
 })
