0a0094af98387d1041bd840a7f1d6f75660cac16
zhengbigbig <780357902@qq.com>
2019-05-14 18:34:53 +0800

    再次重构classes

--- lib/helpers/classes.tsx
+++ lib/helpers/classes.tsx
@@ -6,21 +6,19 @@ interface Options {
   extra: string | undefined;
 }
 
-const scopedClassMaker = (prefix: string) => {
-  return function (name?: string | any[] | ClassToggles, options?: Options) {
-    const name2 = (typeof name === 'string' || name === undefined) ? [name] :
-      name instanceof Array ? Array.from(new Set(name)) :
-        Object.entries(name).filter(item => item[1] !== false).map(item => item[0]);
-    const result = name2.map(item =>
-      [prefix, item].filter(Boolean).join('-')
-    ).join(' ');
-    if (options && options.extra) {
-      return [result, options.extra].filter(Boolean).join(' ');
-    } else {
-      return result;
-    }
-  };
-};
+const scopedClassMaker = (prefix: string) =>
+  (name?: string | any[] | ClassToggles, options?: Options) =>
+    (
+      (typeof name === 'string' || name === undefined) ? [name] :
+        name instanceof Array ? Array.from(new Set(name)) :
+          Object.entries(name)
+            .filter(item => item[1] !== false)
+            .map(item => item[0])
+    )
+      .map(item => [prefix, item].filter(Boolean).join('-'))
+      .concat(options && options.extra || [])
+      .join(' ');
+
 const classes = (...names: (string | undefined)[]) => {
   return names.filter(Boolean).join(' ');
 };
